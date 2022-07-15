e274febf036bc43316c1163772f67bebecbc08c5
Muhammad Hamza <hamzashafique535@gmail.com>
2022-07-16 00:04:31 +0500

    display tracks data

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,4 +1,4 @@
-import { ScrollView, SectionList, Text, View, FlatList, ImageBackground } from 'react-native'
+import { ScrollView, TouchableOpacity, SectionList, Text, View, FlatList, ImageBackground } from 'react-native'
 import React, { Component } from 'react'
 import { FatchTracks } from '../../store/TrackSlice'
 import { connect } from 'react-redux'
@@ -26,27 +26,55 @@ class Home extends Component {
   }
 
   render() {
-    console.log(this.props.tracksData)
     const tData = this.props.tracksData
-    console.log(this.props.tracksData[0].data)
+    const hTime = new Date().getHours()
+    let gText;
+    if (hTime >= 5 & hTime < 12) {
+      gText = 'Good Morning'
+    } else if (hTime >= 12 & hTime < 18) {
+      gText = 'Good Afternoon';
+    } else if (hTime >= 18 & hTime < 21) {
+      gText = 'Good Evening'
+    } else {
+      gText = 'Good Night'
+    }
+
     return (
-
-
       <>
         {this.state.loading ? <SplashScreen /> :
           <View style={Styles.mainView}>
             <ImageBackground style={Styles.bImage} source={require('../../assets/tbgimg.webp')} resizeMode="cover">
+              <View style={Styles.greetingView} >
+                <Text style={Styles.greetingText} >
+                  {gText}
+                </Text>
+              </View>
               <SectionList
                 sections={tData}
                 keyExtractor={item => item.id}
-                renderItem={({ item }) => (
-                  <View>
-                    <Text>{item.id}</Text>
-                  </View>
+                renderSectionHeader={({ section }) => (
+                  <>
+                    <Text style={Styles.titleText}>{section.title}</Text>
+                    <FlatList
+                      horizontal
+                      data={section.data}
+                      renderItem={({ item }) => (
+                        <TouchableOpacity onPress={null}>
+                          <View style={Styles.cardView}>
+                            <ImageBackground style={Styles.cardImg} source={{uri:item.imageUrl}} resizeMode="cover" >
+                                <View style={Styles.imgView1}><Text style={Styles.imgText1} >{item.tName}</Text></View> 
+                                <View style={Styles.imgView2}><Text style={Styles.imgText2}>by: {item.releaseBy}</Text></View> 
+                            </ImageBackground>
+                          </View>
+                        </TouchableOpacity>
+                      )}
+                    />
+                  </>
                 )}
-                renderSectionHeader={({ section: { title } }) => (
-                  <Text >{title}</Text>
-                )}
+                renderItem={({ item, section }) => {
+                  return null;
+                }
+                }
               />
             </ImageBackground>
           </View>
--- src/screens/home/HomeStyle.js
+++ src/screens/home/HomeStyle.js
@@ -1,12 +1,67 @@
-import { StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
 
 export const Styles = StyleSheet.create({
-    mainView:{
-        flex:1,
+    mainView: {
+        flex: 1,
     },
-    bImage:{
-        flex:1,
-        padding:16,
+    bImage: {
+        flex: 1,
+        padding: 16,
+    },
+    greetingView: {
+        marginTop: 8,
+        marginBottom: 16,
+
+    },
+    greetingText: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 22,
+    },
+    titleText: {
+        color: 'Black',
+        fontSize: 18,
+        marginVertical: 16,
+        fontWeight: "bold"
+    },
+    cardView: {
+        borderWidth: 1,
+        height: 170,
+        width: 170,
+        margin: 8
+    },
+    cardImg: {
+        flex: 1,
+        padding: 3,
+    },
+    imgText1: {
+        color: 'black',
+        fontSize: 15,
+        fontWeight: "bold",
+        backgroundColor:'#ffffff90',
+        padding:5,
+        width:140,
+        textAlign:"center",
+        borderRadius:5
+    },
+    imgText2: {
+        backgroundColor:'#00000090',
+        color: 'white',
+        fontSize: 15,
+        fontWeight: "bold",
+        textAlign:"center",
+        padding:3,
+        borderRadius:5,
+        
+    },
+    imgView1: {
+        flex: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imgView2: {
+        flex: 2,
     }
+
 
 })
